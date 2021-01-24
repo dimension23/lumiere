384a4b23315316d6d82c3d5276f682073233c32f
dimension23 <prashant1139@gmail.com>
2021-01-24 00:20:03 +0000

    Added routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,28 @@ import React from "react";
 import SignUp from "./SignUp";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import SignIn from "./SignIn";
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <SignUp />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/signin" component={SignIn} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
